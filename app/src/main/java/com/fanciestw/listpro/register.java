7630344065196561e034252016b2beeefbf147eb
William Lin <wlin26@yahoo.com>
2016-11-20 00:29:42 -0500

    Work in Progress of Registration Activity
    Added FirebaseAuth and got all the needed EditText for user info. Signed
    user out upon starting the registration activity.

--- app/src/main/java/com/fanciestw/listpro/register.java
+++ app/src/main/java/com/fanciestw/listpro/register.java
@@ -2,12 +2,28 @@ package com.fanciestw.listpro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class register extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    private EditText registerEditName = (EditText)findViewById(R.id.registerEditName);
+    private EditText registerEditEmail = (EditText)findViewById(R.id.registerEditEmail);
+    private EditText registerEditPassword = (EditText)findViewById(R.id.registerEditPassword);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        mAuth.signOut();
     }
 }
