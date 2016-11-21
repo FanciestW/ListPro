b2b9d346012fe90b1857bf76d8c8ea56742e44a8
William Lin <wlin26@yahoo.com>
2016-11-20 21:43:05 -0500

    Created User class object

---
+++ app/src/main/java/com/fanciestw/listpro/User.java
@@ -0,0 +1,15 @@
+package com.fanciestw.listpro;
+
+/**
+ * Created by wlin on 11/20/16.
+ */
+
+public class User {
+
+    private String name, email;
+
+    User(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+}
