14920061264bd82f7e136e77fc41323296134fa5
William Lin <wlin26@yahoo.com>
2016-11-22 17:54:17 -0500

    Finished Creating Validations for Registration
    Users will now have their info validated before creating an account.

--- app/src/main/java/com/fanciestw/listpro/register.java
+++ app/src/main/java/com/fanciestw/listpro/register.java
@@ -1,5 +1,6 @@
 package com.fanciestw.listpro;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
@@ -13,6 +14,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -55,6 +59,7 @@ public class register extends AppCompatActivity {
         registerEditEmail.setText("");
         registerEditPassword.setText("");
         mAuth.addAuthStateListener(mAuthListener);
+        mAuth.signOut();
     }
 
     @Override
@@ -63,33 +68,89 @@ public class register extends AppCompatActivity {
         if(mAuthListener != null) mAuth.removeAuthStateListener(mAuthListener);
     }
 
-    public void createUser(View view){
+    public void createUser(View view) throws FirebaseAuthUserCollisionException{
         //TODO::Validate user inputted info
         if(validateInfo()) {
             final String name = registerEditName.getText().toString();
             final String email = registerEditEmail.getText().toString();
             final String password = registerEditPassword.getText().toString();
             Log.d("User Info", "Name: " + name + "\tEmail: " + email + "\tPassword: " + password);
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
             mAuth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
-                            User newUser = new User(name, email);
-                            String uid = task.getResult().getUser().getUid();
-                            mDatabase.child(uid).setValue(newUser);
-                            signUserIn();
+                            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.dismiss();
+                                }
+                            });
+
+                            if(task.isSuccessful()) {
+                                User newUser = new User(name, email);
+                                String uid = task.getResult().getUser().getUid();
+                                mDatabase.child(uid).setValue(newUser);
+                                signUserIn();
+                            } else {
+                                try{
+                                    throw task.getException();
+                                } catch(FirebaseAuthUserCollisionException ex){
+                                    builder.setMessage("There exists an account with this email")
+                                            .setTitle("Email In Use");
+                                    AlertDialog dialog = builder.create();
+                                    dialog.show();
+                                } catch(FirebaseAuthWeakPasswordException ex){
+                                    builder.setMessage(ex.getReason())
+                                            .setTitle("Weak Password");
+                                    AlertDialog dialog = builder.create();
+                                    dialog.show();
+                                } catch(Exception ex){
+                                    builder.setMessage("Exception Caught")
+                                            .setTitle(ex.getMessage());
+                                    AlertDialog dialog = builder.create();
+                                    dialog.show();
+                                }
+                            }
                         }
                     });
         }
     }
 
     public boolean validateInfo(){
-        if(registerEditName.getText().toString() == ""){
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        String name = registerEditName.getText().toString();
+        String email = registerEditEmail.getText().toString();
+        String password = registerEditPassword.getText().toString();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        if(name.equals("")){
             builder.setMessage("Please enter your name")
                     .setTitle("Error");
             AlertDialog dialog = builder.create();
             dialog.show();
+            return false;
+        }
+        if(email.equals("") && !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            builder.setMessage("Please enter a valid email")
+                    .setTitle("Error");
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+            return false;
+        }
+        if(password.equals("")){
+            builder.setMessage("Please enter a password")
+                    .setTitle("Error");
+            AlertDialog dialog = builder.create();
+            dialog.show();
+            return false;
         }
         return true;
     }
@@ -103,5 +164,9 @@ public class register extends AppCompatActivity {
         } catch (NullPointerException ex){
             Log.d("User Status: ", "User no signed in");
         }
+    }
+
+    public void goToLogin(){
+        //TODO::Create function to go to login activity
     }
 }
