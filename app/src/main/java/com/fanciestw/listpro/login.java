58d21bfe97175f6de24237fca159c8bff11b609d
William Lin <wlin26@yahoo.com>
2016-11-23 14:32:16 -0500

    Progress made with Login Activity
    Added FirebaseAuth to Login activity. Started working on logging user
    in.

--- app/src/main/java/com/fanciestw/listpro/login.java
+++ app/src/main/java/com/fanciestw/listpro/login.java
@@ -1,19 +1,62 @@
 package com.fanciestw.listpro;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class login extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    private EditText loginEditEmail = (EditText)findViewById(R.id.loginEditEmail);
+    private EditText loginEditPassword = (EditText)findViewById(R.id.loginEditPassword);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("Login Activity", "onCreate");
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    Log.d("User Activity", "User Signed In");
+                } else {
+                    Log.d("User Activity", "User Sign Out");
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
         Log.d("Login Activity", "onStart");
+        loginEditEmail.setText("");
+        loginEditPassword.setText("");
+        mAuth.addAuthStateListener(mAuthStateListener);
+        mAuth.signOut();
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
         Log.d("Login Activity", "onStop");
+        if(mAuthStateListener != null) mAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
+    public void login(View view){
+        //TODO::Log user in
     }
 
     public void goToRegister(View view){
