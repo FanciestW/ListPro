da7512cc92a6219ce4b47cb800e5813acb12a499
William Lin <wlin26@yahoo.com>
2016-11-24 00:17:43 -0500

    Completed login activity
    Login Activity finished. Some testing may be needed. Login works as
    intended.

--- app/src/main/java/com/fanciestw/listpro/login.java
+++ app/src/main/java/com/fanciestw/listpro/login.java
@@ -1,30 +1,38 @@
 package com.fanciestw.listpro;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseUser;
 
 public class login extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
     private FirebaseAuth.AuthStateListener mAuthStateListener;
 
-    private EditText loginEditEmail = (EditText)findViewById(R.id.loginEditEmail);
-    private EditText loginEditPassword = (EditText)findViewById(R.id.loginEditPassword);
+    private EditText loginEditEmail;
+    private EditText loginEditPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("Login Activity", "onCreate");
         setContentView(R.layout.activity_login);
-        mAuth = FirebaseAuth.getInstance();
+        Log.d("Login Activity", "onCreate");
+        loginEditEmail = (EditText)findViewById(R.id.loginEditEmail);
+        loginEditPassword = (EditText)findViewById(R.id.loginEditPassword);
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -57,6 +65,48 @@ public class login extends AppCompatActivity {
 
     public void login(View view){
         //TODO::Log user in
+        final String email = loginEditEmail.getText().toString();
+        final String password = loginEditPassword.getText().toString();
+        Log.d("User Login Info", "Email: " + email + " Password: " + password);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.dismiss();
+                            }
+                        });
+                        if(task.isSuccessful()) {
+                            Log.d("Login Successful", "Signed in with email: " + email);
+                            Intent intent = new Intent(getBaseContext(), allList.class);
+                            startActivity(intent);
+                        } else {
+                            Log.d("Login Un-successful", "Sign in with email: " + email + " failed");
+                            try{
+                                throw task.getException();
+                            } catch(FirebaseAuthInvalidCredentialsException ex){
+                                builder.setMessage("Invalid Credentials")
+                                        .setTitle("Sorry");
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            } catch(FirebaseAuthException ex){
+                                builder.setMessage("FirebaseAuth Exception")
+                                        .setTitle(ex.getMessage());
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            } catch(Exception ex){
+                                builder.setMessage("Exception Caught")
+                                        .setTitle(ex.getMessage());
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            }
+                        }
+                    }
+                });
     }
 
     public void goToRegister(View view){
