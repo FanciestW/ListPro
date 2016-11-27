c961c71d143622c492a203cf99c1d723d23d2604
William Lin <wlin26@yahoo.com>
2016-11-26 23:02:27 -0500

    Updated List object
    Added ArrayList of UID, will be used for List Sharing. And also a
    dateCreated data member

--- app/src/main/java/com/fanciestw/listpro/List.java
+++ app/src/main/java/com/fanciestw/listpro/List.java
@@ -1,17 +1,25 @@
 package com.fanciestw.listpro;
 
-import java.util.Date;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
+
+import java.util.ArrayList;
 
 /**
  * Created by wlin on 11/12/16.
  */
 
 public class List {
-    private String listTitle, listDescription;
+
+    public String listTitle, listDescription, dateCreated;
+    public ArrayList<String> share = new ArrayList<>();
 
     public List(String title, String descr){
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
         this.listTitle = title;
         this.listDescription = descr;
+        this.dateCreated = format.format(cal.getTime());
     }
 
     public String getTitle() { return this.listTitle; }
