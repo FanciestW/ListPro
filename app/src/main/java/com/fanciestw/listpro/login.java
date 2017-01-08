1f4ca86e8e3558274113ef8d4d6183464a13dd80
William Lin <wlin26@yahoo.com>
2017-01-07 21:23:20 -0500

    Removed TODO because TODO completed

--- app/src/main/java/com/fanciestw/listpro/login.java
+++ app/src/main/java/com/fanciestw/listpro/login.java
@@ -64,7 +64,6 @@ public class login extends AppCompatActivity {
     }
 
     public void login(View view){
-        //TODO::Log user in
         final String email = loginEditEmail.getText().toString();
         final String password = loginEditPassword.getText().toString();
         Log.d("User Login Info", "Email: " + email + " Password: " + password);
