86ece645ccc128cab8aa87649643ebc3a9ac43ea
William Lin <wlin26@yahoo.com>
2017-01-16 15:57:33 -0500

    Removed imports for un-used packages and libraries

--- app/src/main/java/com/fanciestw/listpro/allList.java
+++ app/src/main/java/com/fanciestw/listpro/allList.java
@@ -6,15 +6,12 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.InputType;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -25,7 +22,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class allList extends AppCompatActivity {
 
