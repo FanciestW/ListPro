1acd5263137d665fdbacb66d548ed1117e6b6634
William Lin <wlin26@yahoo.com>
2017-01-17 10:35:19 -0500

    Receives intent extras and uses it to populate list info
    listDetail will now receive the clicked list and populate itself with
    the info on the list

--- app/src/main/java/com/fanciestw/listpro/listDetails.java
+++ app/src/main/java/com/fanciestw/listpro/listDetails.java
@@ -3,6 +3,8 @@ package com.fanciestw.listpro;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class listDetails extends AppCompatActivity {
 
@@ -10,12 +12,11 @@ public class listDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_details);
-    }
-
-    @Override
-    public void onStart(){
-        Intent intent = getIntent();
-        int position = intent.getIntExtra("Position", -1);
-        List clickedList = (List)intent.getSerializableExtra("List");
+        TextView listTitleText = (TextView)findViewById(R.id.listDetail_Title);
+        TextView listDescText = (TextView)findViewById(R.id.listDetail_Description);
+        List list = getIntent().getParcelableExtra("ListParcelable");
+        Log.d("List Name:", list.getListTitle());
+        listTitleText.setText(list.getListTitle());
+        listDescText.setText(list.getListDescription());
     }
 }
