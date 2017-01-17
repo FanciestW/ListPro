ec71de3eca53c88d161490e326242bba646de07a
William Lin <wlin26@yahoo.com>
2017-01-17 11:55:47 -0500

    Added Date Created to view with formatted date

--- app/src/main/java/com/fanciestw/listpro/listDetails.java
+++ app/src/main/java/com/fanciestw/listpro/listDetails.java
@@ -1,10 +1,14 @@
 package com.fanciestw.listpro;
 
 import android.content.Intent;
+import android.icu.text.SimpleDateFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+
+import java.text.ParseException;
+import java.util.Date;
 
 public class listDetails extends AppCompatActivity {
 
@@ -14,9 +18,21 @@ public class listDetails extends AppCompatActivity {
         setContentView(R.layout.activity_list_details);
         TextView listTitleText = (TextView)findViewById(R.id.listDetail_Title);
         TextView listDescText = (TextView)findViewById(R.id.listDetail_Description);
+        TextView listDateText = (TextView)findViewById(R.id.listDetail_DateCreated);
         List list = getIntent().getParcelableExtra("ListParcelable");
         Log.d("List Name:", list.getListTitle());
         listTitleText.setText(list.getListTitle());
         listDescText.setText(list.getListDescription());
+//        String date = list.getDateCreated();
+//        SimpleDateFormat parser = new SimpleDateFormat("yyyy-MM-dd");
+//        Date newDate = parser.parse(date);
+//        SimpleDateFormat formatter = new SimpleDateFormat()
+        SimpleDateFormat formatter = new SimpleDateFormat("MMM dd, yyyy");
+        try {
+            Date date = new SimpleDateFormat("EEE MMM d HH:mm:ss zzz yyyy").parse(list.getDateCreated());
+            listDateText.setText("Date Created: " + formatter.format(date));
+        } catch(ParseException ex){
+
+        }
     }
 }
