9ea95db73c378eac368e00560298ece7efbd84e6
William Lin <wlin26@yahoo.com>
2017-01-17 10:33:29 -0500

    Implemented parcelable to List object
    This will allow List objects to be converted to a parcelable and pass
    through an intent.

--- app/src/main/java/com/fanciestw/listpro/List.java
+++ app/src/main/java/com/fanciestw/listpro/List.java
@@ -2,6 +2,8 @@ package com.fanciestw.listpro;
 
 import android.icu.text.SimpleDateFormat;
 import android.icu.util.Calendar;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 import java.util.ArrayList;
 
@@ -9,7 +11,7 @@ import java.util.ArrayList;
  * Created by wlin on 11/12/16.
  */
 
-public class List {
+public class List implements Parcelable {
 
     public String listTitle, listDescription, dateCreated, user, listID;
 
@@ -34,4 +36,33 @@ public class List {
     public String getDateCreated(){ return this.dateCreated; }
     public String getUser(){ return this.user; }
     public String getListID(){ return this.listID; }
+
+    public static final Parcelable.Creator<List> CREATOR = new Creator<List>(){
+
+        public List createFromParcel(Parcel source){
+            List list = new List();
+            list.listTitle = source.readString();
+            list.listDescription = source.readString();
+            list.dateCreated = source.readString();
+            list.user = source.readString();
+            list.listID = source.readString();
+            return list;
+        }
+
+        public List[] newArray(int size){
+            return new List[size];
+        }
+    };
+
+    public int describeContents(){
+        return 0;
+    }
+
+    public void writeToParcel(Parcel parcel, int flags){
+        parcel.writeString(listTitle);
+        parcel.writeString(listDescription);
+        parcel.writeString(dateCreated);
+        parcel.writeString(user);
+        parcel.writeString(listID);
+    }
 }
