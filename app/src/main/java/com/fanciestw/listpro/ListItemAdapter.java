11b2552a64925af70b081af0a49016da776f5230
William Lin <wlin26@yahoo.com>
2017-03-16 20:36:52 -0400

    Created custom adapter for list items

---
+++ app/src/main/java/com/fanciestw/listpro/ListItemAdapter.java
@@ -0,0 +1,56 @@
+package com.fanciestw.listpro;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by William on 3/16/2017.
+ */
+
+public class ListItemAdapter extends ArrayAdapter<List> {
+    Context context;
+    int layoutResourceId;
+    ArrayList<List> allList = null;
+
+    public ListItemAdapter(Context context, ArrayList<List> allList){
+        super(context, 0, allList);
+        this.context = context;
+        this.layoutResourceId = layoutResourceId;
+        this.allList = allList;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent){
+        //Get the data item for this position
+        final List list = allList.get(position);
+        //Check if an existing view is being reused, otherwise inflate the view
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.all_list_item_layout, parent, false);
+        }
+        //Lookup view for data population
+        TextView tvTitle = (TextView)convertView.findViewById(R.id.list_item_title);
+        TextView tvDesc = (TextView)convertView.findViewById(R.id.list_item_desc);
+        //Populate the date into the template view using the data object
+        tvTitle.setText(list.listTitle);
+        tvDesc.setText(list.listDescription);
+        //Return the completed view to render on screen
+
+        ImageView deleteBtn = (ImageView)convertView.findViewById(R.id.itemdelete);
+        deleteBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(context instanceof allList){
+                    ((com.fanciestw.listpro.allList)context).deleteList(position, list);
+                }
+            }
+        });
+        return convertView;
+    }
+}
