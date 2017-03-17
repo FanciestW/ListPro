0d95e04a857b27e17b666970800eb4c61811b80a
William Lin <wlin26@yahoo.com>
2017-03-16 21:00:55 -0400

    Used list item layout for the list item adapter

--- app/src/main/java/com/fanciestw/listpro/ListItemAdapter.java
+++ app/src/main/java/com/fanciestw/listpro/ListItemAdapter.java
@@ -32,7 +32,7 @@ public class ListItemAdapter extends ArrayAdapter<ListItem> {
         final ListItem listItem = allList.get(position);
         //Check if an existing view is being reused, otherwise inflate the view
         if(convertView == null){
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.all_list_item_layout, parent, false);
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_items_layout, parent, false);
         }
         //Lookup view for data population
         TextView tvTitle = (TextView)convertView.findViewById(R.id.list_item_title);
